3fc9fe8103573017165b77360a9de4436954a5eb
salman_siddeeque <salmanres@gmail.com>
2025-07-25 14:41:15 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,53 @@ app.get('/', (req, res)=>{
     res.send('hello');
 });
 
+//crud operations c-create r-read u-update d-delete
+
 app.post('/register', async (req, res)=>{
     try{
         console.log(req.body);
         await userdata.insertOne(req.body);
         res.status(200).json({message:"user registration successful!"});
+    }catch(err){
+        console.log(err);
+    };
+});
+
+app.get('/userdata', async(req, res)=>{
+    try{
+        const result = await userdata.find();
+        res.send(result);
+    }catch(er){
+        console.log(er);
+    };
+});
+
+app.delete('/deleteuser/:id', async(req, res)=>{
+    try{
+        console.log(req.params);
+        const {id} = req.params;
+        await userdata.findByIdAndDelete(id);
+        res.status(200).json({message :"user data deleted"});
+    }catch(err){
+        console.log(err);
+    };
+});
+
+app.get('/singleruserdata/:id', async(req,res)=>{
+    try{
+        const {id}= req.params;
+        const result = await userdata.findOne({_id : id});
+        res.send(result);
+    }catch(err){
+        console.log(err);
+    };
+});
+
+app.put('/updateuser/:id', async (req,res)=>{
+    try{
+        const {id} = req.params;
+        const result = await userdata.findByIdAndUpdate(id, req.body);
+        res.status(200).json({message: "user details updated!"});
     }catch(err){
         console.log(err);
     };
